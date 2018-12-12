1401a331f8bc5d4ace560ce5e70eb9d4ed30046f
Leana0803 <45233178+Leana0803@users.noreply.github.com>
2018-12-12 11:11:53 +0100

    Delete import Mib.py

--- import Mib.py
+++
@@ -1,3 +0,0 @@
-triples = { (alien.No_Cabine, alien.Nom, gardien.Nom)
-              for alien in baseAlien for gardien in baseGardien
-              if gardien.No_Cabine == alien.No_Cabine}               
